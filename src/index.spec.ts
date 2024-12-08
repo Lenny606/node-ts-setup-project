3c9bd36c04535ea968494b3334c852fc4afb8346
--help <you@example.com>
2024-12-08 15:03:03 +0100

    fix lints

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,18 +1,18 @@
 import supertest from "supertest";
-import {server} from "./index";
+import { server } from "./index";
 
 describe("Server", () => {
-    const req = supertest.agent(server)
+  const req = supertest.agent(server);
 
-    afterAll((done) => {
-        server.close(done)
-    })
-    it("should get /", async () => {
-        const res = await req.get('/')
+  afterAll((done) => {
+    server.close(done);
+  });
+  it("should get /", async () => {
+    const res = await req.get("/");
 
-        expect(res.status).toBe(200)
-        expect(res.body).toEqual({
-            data: "Hello, World!"
-        })
-    })
-})
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      data: "Hello, World!",
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import * as fs from 'fs';
-import http from 'http';
+import * as fs from "fs";
+import http from "http";
 
 const file = fs;
 
 export const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "application/json"
-    })
-    res.end(
-        JSON.stringify({
-            data: "Hello, World!"
-        })
-    )
-})
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+  });
+  res.end(
+    JSON.stringify({
+      data: "Hello, World!",
+    }),
+  );
+});
 
 server.listen(8002, () => {
-    console.log("listening on 8002")
-})
+  console.log("listening on 8002");
+});
